c62b25de49553c2a5de60b4f4ceb4668843ef884
Guilherme Oliveira <guilherme.pinto.de.oliveira1@gmail.com>
2022-09-11 17:35:34 +0200

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version = '0.2.0',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     author = 'CitrusBoy',                   # Type in your name
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+    download_url = 'https://github.com/GuiOliv/HangmanProject/archive/refs/tags/0.2.0.tar.gz',    # I explain this later on
 )
